fdff5862d6022cb40212a55b6337cb79f8159091
Zipsqu <163416599+Zipsqu@users.noreply.github.com>
2025-01-17 20:48:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const outputText = document.getElementById('outputText');
 
 // Add an event listener to the button to handle clicks
 button.addEventListener('click', function() {
-    outputText.textContent = "You've clicked the button! The text has changed.";
+    outputText.textContent = "Haha, you're still a poo poo.";
 });
